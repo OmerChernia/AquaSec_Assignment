952f9ec1653462d709e4c208f17128f50d3fd38a
OmerChernia <omer12899@gmail.com>
2025-06-10 12:20:33 +0300

    Add User class to main.py for user data management

--- main.py
+++ main.py
@@ -1,6 +1,42 @@
 from fastapi import FastAPI
+from pydantic import BaseModel, Field
+from typing import List, Dict
+import json
 
 app = FastAPI()
+
+class UserBase(BaseModel):
+    id: str = Field(..., min_length=9, max_length=9, description="The user's ID")
+    name: str = Field(..., min_length=2, description="The user's name")
+    phone: str = Field(..., description="The user's phone number")
+    address: str = Field(..., description="The user's address")
+
+class UserCreate(UserBase):
+    pass
+
+class userManager:
+    def __init__(self):
+        self.users: Dict[str, UserBase] = {}
+        self.load_users()
+        
+    def load_users(self):
+        try:
+            with open("users.json", "r") as file:
+                self.users = json.load(file)
+                
+        except FileNotFoundError:
+            print("No users file found")
+            
+        except json.JSONDecodeError:
+            print("Error parsing users file")
+            
+    def get_all_users(self):
+        return list(self.users.values())
+    
+    def get_user_by_id(self, id: str):
+        return self.users.get(id)
+            
+
 
 @app.get("/")
 def read_root():
